082ce847a3c9e907e803b522235ed3324af64fca
yukang <youlo0018@163.com>
2026-08-07 12:57:19 +0800

    1

---
+++ src/CommunalService/CommunalService/CommunalService.Domain/Entity/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace CommunalService.Domain.Entity;
+
+public class BaseEntity
+{
+    /// <summary>
+    /// ID
+    /// </summary>
+    [Column(IsIdentity = true, IsPrimary = true)]
+    public long Id { get; set; }
+    /// <summary>
+    /// 是否已删除
+    /// </summary>
+    public bool IsDeleted { get; set; }
+    /// <summary>
+    /// 创建事件
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+    /// <summary>
+    /// 更新时间
+    /// </summary>
+    public DateTime UpdatedAt { get; set; }
+    /// <summary>
+    /// 删除时间
+    /// </summary>
+    public DateTime DeletedAt { get; set; }
+    
+}
