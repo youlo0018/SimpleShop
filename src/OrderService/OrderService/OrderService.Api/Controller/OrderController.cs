e97d9175e5b3a081a48bdea706efceb2cb44cc9b
yuke <youlo0018@163.com>
2026-08-09 15:15:13 +0800

    1

--- src/OrderService/OrderService/OrderService.Api/Controller/OrderController.cs
+++ src/OrderService/OrderService/OrderService.Api/Controller/OrderController.cs
@@ -11,15 +11,6 @@ public class OrderController(IFreeSql freeSql,IOrderRepository orderRepository) 
     [HttpGet]
     public IActionResult Index()
     {
-        freeSql.CodeFirst.SyncStructure<Order>();
-        freeSql.Insert<Order>(new Order()
-        {
-            Id = 1,
-            OrderNo =  Guid.NewGuid().ToString("N"),
-            CustomerNo = Guid.NewGuid().ToString("N"),
-            
-            
-        }).ExecuteAffrows();
         return Ok();
     }
     [HttpGet]
